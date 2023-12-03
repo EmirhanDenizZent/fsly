be9608aff163abb985f31d3b57166d4a9d8da589
Your Name <you@example.com>
2023-12-03 12:08:36 +0300

    js

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("Merhaba");
+
+let firstName = "Deniz";
+
+console.log(firstName);
+
+const lastName = "Zent";
+
+console.log(lastName);
+
+firstName = lastName;
+
+console.log(firstName);
+
+console.log("Soyad:", lastName)
+
+firstName = "Deniz" + " " + "Zent";
+
+console.log("Ad Soyad:", firstName)
+
+
