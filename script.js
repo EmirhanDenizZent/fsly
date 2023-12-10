8af307ffe1b995fad3a3ebd852da2a8cd12a88f5
Your Name <you@example.com>
2023-12-10 10:59:15 +0300

    momentjs

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ birthYear = year - age;
 console.log("Ad Soyad Yaş Doğum Yılı:",firstName + " " + lastName+ " " + age+ " " + birthYear);
 
 let sayi4 = 14;
-let sayi5 =10;
+let sayi5 = 10;
 
 let isBigger = sayi4 > sayi5; //Büyüktür işareti: doğruysa "True" olarak gözükür yanlış ise "False" olarak gözükür.
 
@@ -32,3 +32,42 @@ console.log(isBigger);
 let isLower = sayi4 < sayi5; //Küçüktür işareti: doğruysa "True" olarak gözükür yanlış ise "False" olarak gözükür.
 
 console.log(isLower);
+
+console.log(sayi4 > 10);
+
+sayi4 = 25;
+
+isBigger = sayi4 > sayi5 && sayi5 > sayi4
+
+console.log(isBigger)
+
+let dateVar = new Date();
+
+console.log(dateVar);
+
+const yearInfo = dateVar.getFullYear();
+console.log("Yıl:",yearInfo);
+
+const monthInfo = dateVar.getMonth();
+console.log("Ay:",monthInfo);
+
+const dayInfo = dateVar.getDay();
+console.log("Gün:",dayInfo);
+
+const hourInfo = dateVar.getHours();
+console.log("Saat:",hourInfo);
+
+const minuteInfo = dateVar.getMinutes();
+console.log("Dakika:",minuteInfo);
+
+const secondsInfo = dateVar.getSeconds();
+console.log("Saniye:",secondsInfo);
+
+const millisecondInfo = dateVar.getMilliseconds();
+console.log("Milisaniye:",millisecondInfo);
+
+const timeInfo = dateVar.getTime();
+console.log("Toplam Milisaniye:",timeInfo);
+
+const momnetDate = moment();
+console.log(momnetDate);
