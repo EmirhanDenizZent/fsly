eab68d2e5b3e809abf17a3df646d23809cc78c34
Your Name <you@example.com>
2023-12-16 12:43:23 +0300

    function

---
+++ scripts/script_4.js
@@ -0,0 +1,32 @@
+function consolaYazdir() {
+  console.log("Fonksiyon çalıştı ve console'a yazıldı!");
+};
+
+const consolaYazdir2 = () => {
+    console.log("Arrow fucntion çalıştı!");
+};
+
+function consolaYazdir3(parameter1, parameter2) {
+    console.log("Hava durumu:", parameter1);
+    console.log("Hava sıcaklığı:", parameter2);
+}
+
+consolaYazdir();
+consolaYazdir2();
+
+consolaYazdir3("Rainy", 11);
+
+
+function toplamaIslemi (sayi1, sayi2) {
+    const toplam = sayi1 + sayi2;
+    console.log("Sayıların toplamı", toplam);
+}
+
+toplamaIslemi(150, 55);
+
+function I (A, B) {
+    const bölme = A / B;
+    console.log("Bölme işleminin sonucu:", bölme)
+}
+
+I (100, 25);
