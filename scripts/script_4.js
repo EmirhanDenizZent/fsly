bad1bbef26dfa8d47955a415d37419d241e4bb52
EmirhanDenizZent <emirhandenizzent05@gmail.com>
2023-12-17 10:17:20 +0300

    change

--- scripts/script_4.js
+++ scripts/script_4.js
@@ -1,14 +1,14 @@
 function consolaYazdir() {
   console.log("Fonksiyon çalıştı ve console'a yazıldı!");
-};
+}
 
 const consolaYazdir2 = () => {
-    console.log("Arrow fucntion çalıştı!");
+  console.log("Arrow fucntion çalıştı!");
 };
 
 function consolaYazdir3(parameter1, parameter2) {
-    console.log("Hava durumu:", parameter1);
-    console.log("Hava sıcaklığı:", parameter2);
+  console.log("Hava durumu:", parameter1);
+  console.log("Hava sıcaklığı:", parameter2);
 }
 
 consolaYazdir();
@@ -16,17 +16,16 @@ consolaYazdir2();
 
 consolaYazdir3("Rainy", 11);
 
-
-function toplamaIslemi (sayi1, sayi2) {
-    const toplam = sayi1 + sayi2;
-    console.log("Sayıların toplamı", toplam);
+function toplamaIslemi(sayi1, sayi2) {
+  const toplam = sayi1 + sayi2;
+  console.log("Sayıların toplamı", toplam);
 }
 
 toplamaIslemi(150, 55);
 
-function I (A, B) {
-    const sonuc = A / B;
-    console.log("Bölme işleminin sonucu:", sonuc)
+function I(A, B) {
+  const sonuc = A / B;
+  console.log("Bölme işleminin sonucu:", sonuc);
 }
 
-I (100, 25);
+I(100, 25);
