01636cbd0a1b4d0a19c94f40a2a16ad0bf9ade25
Your Name <you@example.com>
2024-01-14 12:34:39 +0300

    s

--- scripts/script_8.js
+++ scripts/script_8.js
@@ -6,9 +6,6 @@ console.log(document);
 const h2Elm = document.getElementById("baslik");
 console.log(h2Elm);
 
-let imageElm = document.getElementById("random-gorsel");
-console.log(imageElm);
-
 const divElm = document.getElementById("selam-ver");
 console.log(divElm);
 
@@ -45,5 +42,44 @@ let degisiklik = document.querySelectorAll(".eleman");
 console.log(degisiklik);
 
 degisiklik.forEach((item) => {
-    item.innerText = "Değişiklik"
+  item.innerText = "Değişiklik";
 });
+
+// setTimeOut metodu: Bir fonksiyonu çalışmasını belirli bir zaman kadar ötelemek için kullanılır.
+
+function sayHello() {
+  console.log("Hello World!");
+}
+
+// sayHello(); Doğrudan çağırılırsa fonksiyon hemen çalışacaktır.
+
+setTimeout(sayHello, 5000); // ilk parametre çalışacak fonksiyon, ikinci parametre ise ne kadar süre sonra çalışacağıdır.
+//Bu süre milisaniye olmalıdır. (1 saniye = 1000 milisaniye)
+
+// setIntervel metodu: Bir fonksiyonu belirli bir periyodlarda sürekli çalıştırmayı sağlar. Örneğin fonksiyonumuzun her 5 saniyede bir çalışması gibi
+
+// setInterval(function(){
+//   console.log("Her 6 saniyede bir çalıştım");
+// }, 6000);
+
+let i = 0;
+
+const degeriGüncelle = () => {
+  i++;
+  console.log("i değeri güncellendi: ", i);
+  if (i === 5) {
+    clearInterval(sürekliCalis); // interval'ın çalışmasını sonlandırır
+  }
+};
+
+const sürekliCalis = setInterval(degeriGüncelle, 3000);
+
+// DOM elemanları arasında geçiş (parent/sibling/child)
+
+const alinacakUrunEl = document.getElementById("alinacak-urun");
+console.log(alinacakUrunEl);
+
+console.log(alinacakUrunEl.parentNode); // bu yaklaşımla seçilmiş elemanın parent'ı elde edilebilir.
+console.log(alinacakUrunEl.childNodes); // bu yaklaşımla seçilmiş elemanın child'ı elde edilebilir.
+console.log(alinacakUrunEl.nextElementSibling);
+console.log(alinacakUrunEl.previousElementSibling);
